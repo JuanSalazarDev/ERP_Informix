572fe2da2326e0c331240c1161248f85207db944
Juan Salazar <juan.salazar.dev@gmail.com>
2026-04-21 17:22:36 -0500

    feat: Mejoras en legibilidad del script

--- scripts/arranque_sistema.sh
+++ scripts/arranque_sistema.sh
@@ -1,15 +1,39 @@
 #!/bin/bash
 
-#fecha=$(date '+%Y%m%d')
-archivo_usuario=/tmp/log_${$}_$(date '+%Y%m%d')
+usuario=""
+
+#
+# ============================================================================================================
+# Ejecutar login ERP
+# ============================================================================================================
+#
+ejecutar_login(){
+
+local archivo_usuario=/tmp/log_${$}_$(date '+%Y%m%d')
 
 fglgo ./bin/login/login.4gi ${archivo_usuario}
 
 if [[ $? -ne 0 ]]; then
-	echo "Error al ingresar"
-	exit 1
+	return 1
 fi
 
 usuario=$(cat ${archivo_usuario})
+rm -rf ${archivo_usuario} 2> /dev/null
 
-rm -rf ${archivo_usuario}
+if [[ -z "${usuario}" ]]; then
+	return 1
+fi
+
+return 0
+
+}
+#
+# ============================================================================================================
+# Inicio ejecucion script
+# ============================================================================================================
+#
+if ! ejecutar_login; then
+	exit 1
+fi
+
+if ! gestionar_sesiones
