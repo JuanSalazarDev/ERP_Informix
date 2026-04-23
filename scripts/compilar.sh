9e6fa20a845c531ea2de23057d736d72a63e55da
Juan Salazar <juan.salazar.dev@gmail.com>
2026-04-22 21:03:08 -0500

    Refactor: Refactorización script de compilación

--- scripts/compilar.sh
+++ scripts/compilar.sh
@@ -6,13 +6,20 @@ find . -type f -name "*.4gl" | xargs -I {} fglpc {}
 # Generar archivos .frm
 find . -type f -name "*.per" | xargs -I {} form4gl {}
 
-mv servicios_de_claves.4go bin/biblioteca/4go.d
-mv login.4go bin/login/4go.d
-mv main_login.4go bin/login/4go.d
-mv claves_usuarios.4go bin/tablas
-mv usuarios.4go bin/tablas
-mv src/login/per.d/login.frm bin/login/frm.d
-mv src/biblioteca/per.d/cambiar_clave.frm bin/biblioteca/frm.d
+# Mover formularios .frm
+for archivo_frm in $(find . -type f -name "*.frm"); do
+	directorio=$(echo ${archivo_frm} | cut -d "/" -f 3)
+
+	mv ${archivo_frm} ./bin/${directorio}/frm.d
+done
+
+# Mover archivos .4gl
+for archivo_4gl in $(find . -type f -name "*.4gl"); do
+	archivo_4go="$(echo ${archivo_4gl} | awk -F "/" '{print $NF}' | cut -d "." -f 1).4go"
+	directorio=$(echo ${archivo_4gl} | cut -d "/" -f 3)
+
+	mv ./${archivo_4go} bin/${directorio}/4go.d/
+done
 
 cat bin/login/4go.d/login.4go bin/login/4go.d/main_login.4go bin/biblioteca/4go.d/servicios_de_claves.4go \
-	bin/tablas/claves_usuarios.4go bin/tablas/usuarios.4go > bin/login/login.4gi
+	bin/tablas/4go.d/claves_usuarios.4go bin/tablas/4go.d/usuarios.4go > bin/login/login.4gi
